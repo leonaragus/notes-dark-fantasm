c3a5fe569b2ae505c780e53aa02509d098d17364
Leonardo <leonaragus@gmail.com>
2026-02-27 19:23:44 -0300

    fix: convert MainActivity to Java and add missing androidx annotation dependency to resolve CI errors

---
+++ android/app/src/main/java/com/example/ghost_notes_digital_twin/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ghost_notes_digital_twin;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/ghost_notes_digital_twin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ghost_notes_digital_twin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
